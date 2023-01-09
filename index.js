891a69d737f8716eddb261f92bb74014d1e0342e
Dequé <carolinedeque@gmail.com>
2023-01-09 17:44:14 +0100

    adding randomCard function

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  Name: "Caro",
+  Chips: 200,
+};
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
+let playerEl = document.querySelector("#player-el");
+playerEl.textContent = player.Name + ": $" + player.Chips;
 
+// To generate a random card
 function getRandomCard() {
-  return 5;
+  let randomNumber = Math.floor(Math.random() * 13) + 1; // will return a number between 1-13
+
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
+// To start the game
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
+// To display the cards
 function renderGame() {
   cardsEl.textContent = "Cards: ";
   // for loop to display all the cards available in the cards array
@@ -43,10 +63,12 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard();
-
-  sum += card;
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+  }
 }
